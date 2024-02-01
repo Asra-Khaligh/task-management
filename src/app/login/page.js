8042bb2ef2d5dd491448deb95232f3c4227b249d
ItsAAA <imcengineer03@gmail.com>
2024-02-02 01:27:51 +0330

    added submit to login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,8 +8,8 @@ export default function page() {
                <input className={styles.emailInput} type='email' placeholder='Email'></input>
 
                <input className={styles.passwordInput} type='password' placeholder='Password'></input>
-
                
+               <input type='submit' value='login' className={styles.submit}></input>
             </form>
           </div>
          )
