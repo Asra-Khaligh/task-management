029f4f3f4f4f162e8b7e2ac1476a79544c1508d7
ItsAAA <imcengineer03@gmail.com>
2024-02-02 03:22:19 +0330

    managed the entered values to inputs

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,22 @@
+"use client";
+
 import styles from '../page.module.css'
+import { useState } from "react";
 
 export default function page() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
           <div className={styles.loginFormContainer}>
             <form className={styles.loginForm}>
 
-               <input className={styles.emailInput} type='email' placeholder='Email'></input>
+               <input className={styles.emailInput} type='email' placeholder='Email'
+                      onChange={(e) => {setEmail(e.currentTarget.value)}}></input>
 
-               <input className={styles.passwordInput} type='password' placeholder='Password'></input>
+               <input className={styles.passwordInput} type='password' placeholder='Password'
+                      onChange={(e) => {setPassword(e.currentTarget.value)}}></input>
                
                <input type='submit' value='login' className={styles.submit}></input>
             </form>
